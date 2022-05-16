ea0f75af41b156dc9b45abf378ee39d3987f893b
yunfeng <yunfeng.zhang@convertlab.com>
2022-05-16 14:37:08 +0800

    test
    Change-Id: Ib3dc34466180d75598bc7cbe3c4e3f4f0c771fc4

--- src/main/java/com/example/demo/controller/Test02Controller.java
+++ src/main/java/com/example/demo/controller/Test02Controller.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.common.IPUtils;
+import com.example.demo.util.IPUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,9 @@ public class Test02Controller {
 
     @GetMapping("/test03")
     public void test(HttpServletRequest httpServletRequest){
+        System.out.println(IPUtil.getIpFromRequest(httpServletRequest));
+        String ipAddressAtService = IPUtils.getIpAddressAtService(httpServletRequest);
+        System.out.println(ipAddressAtService);
         HttpSession session = httpServletRequest.getSession();
         session.setAttribute("name","bob");
     }
